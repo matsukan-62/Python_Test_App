e2d384b75734917d02239518709b7209e7cb56cd
matsukan <five08016161@gmail.com>
2025-12-19 16:25:42 +0900

    example.pyの作成

---
+++ example.py
@@ -0,0 +1,29 @@
+print("hello world")
+
+
+def greet():
+    print("こんにちは")
+
+
+greet()
+
+
+def print_name(name="name"):
+    print(f'私の名前は{name}です')
+
+
+print_name()
+
+
+def get_greet():
+    return "おはようございます"
+
+
+print(get_greet())
+
+
+def add(a=0, b=0):
+    return a+b
+
+
+print(add(1, 2))
